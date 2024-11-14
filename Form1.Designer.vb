50c659501b68f67a2c135816bfb15f9159f7a65a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-11-14 16:40:10 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        TextBox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(23, 358)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(669, 80)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(713, 358)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 80)
+        Button1.TabIndex = 1
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(130, 29)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.ShortcutsEnabled = False
+        TextBox2.Size = New Size(532, 307)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Collections.Specialized.BitVector32
+Imports LLama
+Imports LLama.Common
+
+Public Class Form1
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim modelPath As String = "C:\Users\admin\Downloads\llama-2-7b-chat.Q2_K.gguf"
+
+        Dim parameters = New ModelParams(modelPath) With {
+            .ContextSize = 1024,
+            .GpuLayerCount = 5
+        }
+
+        Dim model = LLamaWeights.LoadFromFile(parameters)
+        Dim context = model.CreateContext(parameters)
+        Dim executor = New InteractiveExecutor(context)
+
+        Dim session = New ChatSession(executor, New ChatHistory())
+
+        Dim inferenceParams = New InferenceParams() With {
+            .MaxTokens = 256,
+            .AntiPrompts = New List(Of String) From {"User:"}
+        }
+
+        Dim userInput As String = TextBox1.Text
+        TextBox1.Clear()
+
+        TextBox2.AppendText($"User: {userInput}{vbCrLf}Assistant: ")
+
+        Dim response = Await session.ChatAsync(New ChatHistory.Message(AuthorRole.User, userInput),
+                                               inferenceParams).ConfigureAwait(False)
+
+        For Each Text In response
+            TextBox2.AppendText(Text)
+            Application.DoEvents()
+        Next
+
+        TextBox2.AppendText(vbCrLf & vbCrLf)
+    End Sub
+End Class
